344d6feed3b1a50a5ae75aee12c5fcdbf79d0a82
Ildar Galeev <KeinAsylum@gmail.com>
2016-09-26 14:41:14 +0300

    FE-38: Returned keycloak integration

--- app/components/top-panel/top-panel.js
+++ app/components/top-panel/top-panel.js
@@ -2,12 +2,9 @@ const topPanel = angular.module('topPanel', []);
 
 topPanel.component('topPanel', {
     templateUrl: 'components/top-panel/top-panel.html',
-    controller: function () {
-        // this.profileName = Auth.profileName;
-        // this.logout = Auth.logout;
-        // this.token = Auth.token;
-        this.profileName = 'test name';
-        this.logout = () => {};
-        this.token = 'test token';
+    controller: function (Auth) {
+        this.profileName = Auth.profileName;
+        this.logout = Auth.logout;
+        this.token = Auth.token;
     }
 });
--- app/shared/resources/resources.module.js
+++ app/shared/resources/resources.module.js
@@ -1,6 +1,6 @@
 const resources = angular.module('resources', ['ngResource']);
 
 resources.config(function ($httpProvider) {
-    // $httpProvider.interceptors.push('authInterceptor');
+    $httpProvider.interceptors.push('authInterceptor');
     $httpProvider.interceptors.push('dateInterceptor');
 });
