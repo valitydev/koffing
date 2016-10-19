af823b78628c8e7884eb1f73bbd877a6cd6c258e
Ildar Galeev <KeinAsylum@gmail.com>
2016-10-19 14:27:45 +0300

    FE-61: deleted app-config.js (#19)

--- app/components/app/app-config.js
+++
@@ -1,8 +0,0 @@
-// app.constant('appConfig', {
-//     capiUrl: 'http://macroserver1.msk1.rbkmoney.net:8080/v1/',
-//     shopID: 'THRIFT-SHOP'
-// });
-
-app.constant('appConfig', {
-    capiUrl: 'http://localhost:9000/v1/'
-});
