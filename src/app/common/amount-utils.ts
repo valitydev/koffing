2bda2cf77ec559419e6b281c8ff10309d089affa
Alexandra Usacheva <lazercore@me.com>
2018-08-24 12:53:43 +0300

    FE-648: round fix (#215)

--- src/app/common/amount-utils.ts
+++ src/app/common/amount-utils.ts
@@ -1,7 +1,7 @@
 import { isNil, round } from 'lodash';
 
 export const toMinor = (amount: number): number =>
-    isNil(amount) ? null : round(amount, 2) * 100;
+    isNil(amount) ? null : round(amount * 100, 2);
 
 export const toDisplayAmount = (minor: number): number =>
     isNil(minor) ? null : round(minor / 100, 2);
