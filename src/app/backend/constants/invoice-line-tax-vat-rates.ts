506f3f2b1ea1d8f5aac41fbbc7a1947f8dc34d0a
Alexandra Usacheva <lazercore@me.com>
2019-01-11 16:26:15 +0300

    FE-735: Added new tax option and select fix. (#235)

--- src/app/backend/constants/invoice-line-tax-vat-rates.ts
+++ src/app/backend/constants/invoice-line-tax-vat-rates.ts
@@ -2,6 +2,8 @@ export const INVOICE_LINE_TAX_VAT_RATES = {
     percent_0: '0%',
     percent_10: '10%',
     percent_18: '18%',
+    percent_20: '20%',
     value_10_110: '10/110',
-    value_18_118: '18/118'
+    value_18_118: '18/118',
+    value_20_120: '20/120'
 };
